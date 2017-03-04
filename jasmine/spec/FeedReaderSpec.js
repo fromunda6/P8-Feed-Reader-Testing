f26cafb904b896fe13b32658b99a6f42f22edc1b
Goyo <fromunda6@zoho.com>
2017-03-04 16:08:52 -0700

    refactored RSS Feeds, Menu, and Initial Entries tests
    to be more comprehensive and robust

--- jasmine/spec/FeedReaderSpec.js
+++ jasmine/spec/FeedReaderSpec.js
@@ -31,7 +31,7 @@ $(function() {
         it('has defined and nonempty URL values', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
                 expect(allFeeds[i].url).toBeDefined();
-                expect(allFeeds[i].url).not.toBe(null);
+                expect(allFeeds[i].url).not.toBe('');
             }
         });
 
@@ -41,60 +41,42 @@ $(function() {
         it('has defined and nonempty name values', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
                 expect(allFeeds[i].url).toBeDefined();
-                expect(allFeeds[i].url).not.toBe(null);
+                expect(allFeeds[i].url).not.toBe('');
             }
         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        //declare a constant(as opposed to a variable) which cannot be reassigned
+        const relevantClasses = document.body.classList;
+
+         //testing default state of DOM menu visibility
         it('should be hidden on load/by default', function() {
-
-            var htmlBody = $(document.body);
-            var bodyClassName = htmlBody.attr('class');
-
-            expect(bodyClassName).toBe('menu-hidden');
+            expect(relevantClasses).toContain('menu-hidden');
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
-         */
+     //ensure the menu changes visibility-state when clicked
         it('should become visible when first clicked', function() {
-
-            var htmlBody = $(document.body);
             var menuIcon = $(".menu-icon-link");
 
             menuIcon.trigger("click");
-            var bodyClassName = htmlBody.attr('class');
 
-            expect(bodyClassName).toBe('');
+            expect(relevantClasses).not.toContain('menu-hidden');
+
             menuIcon.trigger("click");
 
         });
 
         it('should become hidden when clicked again', function() {
-
-            var htmlBody = $(document.body);
             var menuIcon = $(".menu-icon-link");
 
             menuIcon.trigger("click");
             menuIcon.trigger("click");
-            var bodyClassName = htmlBody.attr('class');
 
-            expect(bodyClassName).toBe('menu-hidden');
+            expect(relevantClasses).toContain('menu-hidden');
         });
     });
-
-    /* TODO: Write a new test suite named "Initial Entries" */
 
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
@@ -103,20 +85,15 @@ $(function() {
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
     describe('Initial Entries Container', function() {
-        var container = $('.feed');
 
         beforeEach(function(done) {
-            loadFeed(0, function() {
-                done();
-            });
+            loadFeed(0, done);
         });
 
         //tests only that html is not empty, not that it in fact contains an entry
-        it('should contain at least 1 entry', function(done) {
-            expect(container.html()).not.toBe('');
-            //further
-            expect(container.length).not.toBe(0);
-            done();
+        it('should contain at least 1 entry', function() {
+            var entries = $('.feed .entry');
+            expect(entries.length).toBeGreaterThan(0);
         });
     });
 
